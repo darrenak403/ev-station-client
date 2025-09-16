da12025237ceb58511086a84a20da699be473927
darrenak403 <ngothanhdat4002@gmail.com>
2025-09-16 22:05:30 +0700

    feat: add MyButton component and integrate into Home page
    - Added MyButton component with customizable variants and styles.
    - Integrated MyButton into the Home page alongside the ThemeToggle component.
    - Updated RootLayout to suppress hydration warnings.
    - Adjusted ThemeToggle button styles for consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 "use client";
 import {ThemeToggle} from "@/components/modules/SwithTheme/theme-toggle";
+import {MyButton} from "@/components/styled/Button/MyButton";
 
 export default function Home() {
   return (
     <div>
-      <div className="fixed top-4 right-4 z-50">
-        <ThemeToggle />
+      <div className="">
+        <ThemeToggle  />
+        <MyButton>Click me</MyButton>
       </div>
     </div>
   );
