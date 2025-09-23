15ec9a4f9e8a9484399b19e35692473d09ed8ba5
ThinhTP204 <tptvt.2002@gmail.com>
2025-09-23 16:32:39 +0700

    feat: implement Google login functionality and update related hooks

---
+++ src/app/(admin)/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboardPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default dashboardPage
--- src/hook/singleton/swrs/SwrProvider.tsx
+++ src/hook/singleton/swrs/SwrProvider.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import {PropsWithChildren, createContext} from "react";
 import { useFetchLoginSwrCore } from "./useFetchLoginSwr";
+import { useFetchLoginGoogleSwrCore } from "./useFetchLoginGoogleSwr";
 
 export interface SwrContextType {
   useFetchLoginSwr: ReturnType<typeof useFetchLoginSwrCore>;
+  useFetchLoginGoogleSwr: ReturnType<typeof useFetchLoginGoogleSwrCore>; // Thêm dòng này
+
 }
 
 export const SwrContext = createContext<SwrContextType | null>(null);
 
 export const SwrProvider = ({children}: PropsWithChildren) => {
   const useFetchLoginSwr = useFetchLoginSwrCore();
+  const useFetchLoginGoogleSwr = useFetchLoginGoogleSwrCore();
   return (
     <>
       <SwrContext.Provider
         value={{
           useFetchLoginSwr,
+          useFetchLoginGoogleSwr,
         }}
       >
         {children}
